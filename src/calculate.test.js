e5e3efbd86724a43fb0c07ae43da65f02decfa0d
William Koller <williamkoller404@gmail.com>
2022-06-23 20:34:01 -0300

    test: make sure x / y is equal to 2

--- src/calculate.test.js
+++ src/calculate.test.js
@@ -1,4 +1,4 @@
-const { sum, sub } = require('./calculate');
+const { sum, sub, div } = require('./calculate');
 
 describe('Calculate Test', () => {
   it('make sure x + y is equal to 5 ', () => {
@@ -7,5 +7,9 @@ describe('Calculate Test', () => {
 
   it('make sure x - y is equal to 4 ', () => {
     expect(sub(8, 4)).toBe(4);
+  });
+
+  it('make sure x / y is equal to 2 ', () => {
+    expect(div(10, 5)).toBe(2)
   });
 });
